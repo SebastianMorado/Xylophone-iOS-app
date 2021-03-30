32e87e34da3c4baa3d99582e4f453a11cb6dd090
SebastianMorado <sabby.morado@gmail.com>
2021-03-31 00:02:06 +0800

    add sound functionality

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(noteName : sender.currentTitle!)
+    }
     
-
+    func playSound(noteName : String) {
+        let url = Bundle.main.url(forResource: noteName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
